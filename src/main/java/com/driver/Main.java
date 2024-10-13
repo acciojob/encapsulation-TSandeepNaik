90e0082167f0690615a452a0b7c10d6234bcef1f
TSandeepNaik <sandeepnaikat100@gmail.com>
2024-10-13 21:35:13 +0530

    encapsulation solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setNum(5);
+        System.out.println("the private num is :"+obj.getNum());
+    }
+
+
+
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int num;
+
+
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
 }
+
+
+
