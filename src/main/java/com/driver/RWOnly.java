c4860a5ac522b492a11af624b1eef9e1d176e636
TSandeepNaik <sandeepnaikat100@gmail.com>
2024-10-14 14:43:43 +0530

    again added

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,16 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    private int num;
+    private String name;
 
 
-
-    public int getNum() {
-        return num;
+    public String getName() {
+        return name;
     }
 
-    public void setNum(int num) {
-        this.num = num;
+    public void setName(String name) {
+        this.name = name;
     }
 }
 
